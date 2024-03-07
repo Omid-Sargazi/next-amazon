556970cfef2f791129ac7d5922771e2a6e897668
OmidSargazi <om.sargazi@gmail.com>
2024-03-08 01:17:56 +0330

    create product type

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,3 +1,5 @@
+import ProductsItem from '@/components/products/ProductsItem'
+import data from '@/lib/models/data'
 import React from 'react'
 
 const Home = () => {
@@ -6,6 +8,9 @@ const Home = () => {
     <h2 className=' text-2xl py-2'>
       Latest Products
     </h2>
+    <div className='grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4'>
+      {data.products.map(product=><ProductsItem key={product.slug} product={product} />)}
+    </div>
     </>
   )
 }
---
+++ lib/models/productModels.ts
@@ -0,0 +1,17 @@
+export type Product = {
+    _id?:string,
+    name:string,
+    slug:string,
+    image:string,
+    banner?:string,
+    price:number,
+    brand:string,
+    description:string,
+    category:string,
+    rating:string,
+    numberReviews:string,
+    countInStock:number,
+    colors?:[],
+    sizes?:[], // sizes and colors are list because they are optional
+}
+ 
